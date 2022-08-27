1aaa69101b9bb16aba0a54adf1cd864e2d505645
Andrew Hewitson <andy.c.hewitson@gmail.com>
2022-08-27 20:02:00 +0200

    Add response class and builder

--- src/response.rs
+++ src/response.rs
@@ -1,2 +1,36 @@
 #[derive(Debug)]
-pub struct Response {}
+pub struct Response {
+    status: u16,
+    body: String,
+}
+
+pub struct ResponseBuilder {
+    status: u16,
+    body: String,
+}
+
+impl ResponseBuilder {
+    pub fn new() -> Self {
+        ResponseBuilder {
+            status: 0,
+            body: String::new(),
+        }
+    }
+
+    pub fn body(mut self, body: String) -> ResponseBuilder {
+        self.body = body;
+        self
+    }
+
+    pub fn status(mut self, status: u16) -> ResponseBuilder {
+        self.status = status;
+        self
+    }
+
+    pub fn build(self) -> Response {
+        Response {
+            status: self.status,
+            body: self.body,
+        }
+    }
+}
