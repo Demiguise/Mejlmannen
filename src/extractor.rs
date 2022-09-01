37981cb152f0396f6c333bded43e4eb78b6f1225
Andrew Hewitson <andy.c.hewitson@gmail.com>
2022-09-01 18:53:22 +0200

    Add some nice failing tests

--- src/extractor.rs
+++ src/extractor.rs
@@ -1,6 +1,6 @@
-use anyhow::{anyhow, Result};
-use crate::response::Response;
 use crate::common::StringMap;
+use crate::response::Response;
+use anyhow::{anyhow, Result};
 
 #[derive(Debug)]
 enum ExtractorTypes {
@@ -23,20 +23,115 @@ mod json {
     use anyhow::{anyhow, Result};
     use serde_json::Value;
 
-    pub fn extract(
-        extract_string: &str,
-        response: &Response,
-    ) -> Result<String> {
+    pub fn extract(extract_string: &str, response: &Response) -> Result<String> {
         println!("JSON Parsing [{}]", extract_string);
         let body = String::from_utf8(response.body().clone())?;
-        let v: Value = serde_json::from_str(&body.as_str())?;
 
-        match v.get(extract_string) {
-            Some(value) => Ok(value.to_string()),
-            None => Err(anyhow!(
-                "Couldn't find [{}] in response body",
-                extract_string
-            )),
+        let mut v: Value = serde_json::from_str(&body.as_str())?;
+
+        for token in extract_string.split('.') {
+            match v.get(token) {
+                Some(value) => {
+                    // TODO: Anyway to _not_ clone this value?
+                    v = value.clone();
+                }
+                None => {
+                    return Err(anyhow!(
+                        "Couldn't find [{}] in the response body. Full Query [{}]",
+                        token,
+                        extract_string
+                    ))
+                }
+            };
+        }
+
+        Ok(v.to_string())
+    }
+
+    #[cfg(test)]
+    mod test {
+        use crate::response::ResponseBuilder;
+
+        fn get_basic_string() -> &'static str {
+            r#"
+        {
+            "name": "John Doe",
+            "age": 43,
+            "phones": [
+                "+44 1234567",
+                "+44 2345678"
+            ]
+        }"#
+        }
+
+        #[test]
+        fn basic_string() {
+            let response = ResponseBuilder::new()
+                .body(get_basic_string().as_bytes().to_vec())
+                .build();
+            let to_extract = "name";
+            let value = super::extract(to_extract, &response);
+            assert!(value.is_ok(), "Extracting failed: {:?}", value.unwrap_err());
+            assert_eq!(value.unwrap(), "\"John Doe\"");
+        }
+
+        #[test]
+        fn basic_int() {
+            let response = ResponseBuilder::new()
+                .body(get_basic_string().as_bytes().to_vec())
+                .build();
+            let to_extract = "age";
+            let value = super::extract(to_extract, &response);
+            assert!(value.is_ok(), "Extracting failed: {:?}", value.unwrap_err());
+            assert_eq!(value.unwrap(), "43");
+        }
+
+        #[test]
+        fn basic_index() {
+            let response = ResponseBuilder::new()
+                .body(get_basic_string().as_bytes().to_vec())
+                .build();
+            let to_extract = "phones[1]";
+            let value = super::extract(to_extract, &response);
+            assert!(value.is_ok(), "Extracting failed: {:?}", value.unwrap_err());
+            assert_eq!(value.unwrap(), "\"+44 2345678\"");
+        }
+
+
+        fn get_deep_object() -> &'static str {
+            r#"
+        {
+            "foo": {
+                "bar": {
+                    "baz": {
+                        "a": {
+                            "b": {
+                                "c": {
+                                    "d": 1066
+                                },
+                                "x": 6
+                            },
+                            "y": 5
+                        },
+                        "z": 4
+                    },
+                    "zab": 3
+                },
+                "rab": 2
+            },
+            "oof": 1
+        }"#
+        }
+
+        #[test]
+        fn deep_object() {
+            let response = ResponseBuilder::new()
+                .body(get_deep_object().as_bytes().to_vec())
+                .build();
+            let to_extract = "foo.bar.baz.a.b.c.d";
+            let value = super::extract(to_extract, &response);
+            assert!(value.is_ok(), "Extracting failed: {:?}", value.unwrap_err());
+            assert_eq!(value.unwrap(), "1066");
         }
     }
 }
